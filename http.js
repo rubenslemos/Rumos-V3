e99ac668b6dcec3a15144a4c9c5304258672d608
rubenslemos <rubenslemos@gmail.com>
2021-08-22 11:57:03 -0300

    formatação de exibição de data

--- http.js
+++ http.js
@@ -2,6 +2,12 @@ const http = require('express');
 const app = http();
 const route = require('./public/route')
 const path = require('path');
+const moment = require("moment");
+
+app.use((req, res, next) => {
+    res.locals.moment = moment;
+    next();
+});
 app.set('view engine', 'ejs');
 app.use(http.static("public"));
 app.set('views', path.join(__dirname, 'views'));
